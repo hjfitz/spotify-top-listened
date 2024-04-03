d13ddc9644687fe061bc331d7fa4af6fdc014148
Harry Fitzgerald <hfitz83@gmail.com>
2024-04-03 19:45:33 +0100

    feat: add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,14 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Spotify Top Listened',
   description: 'A simple app to show your top listened artists and tracks',
+  openGraph: {
+    title: 'Spotify Top Listened',
+    description: 'A simple app to show your top listened artists and tracks',
+    type: 'website',
+    locale: 'en_GB',
+    url: 'https://spotify.hjf.io',
+    images: ['/opengraph.jpeg'],
+  },
 }
 
 export default function RootLayout({
